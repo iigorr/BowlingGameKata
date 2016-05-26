e18f4fa9f6c738f3704d061bdb342e4918c2aa62
Christoph Wagner <github@pandawhisperer.net>
2016-05-26 00:46:19 -0700

    Second working test

--- test/game_spec.js
+++ test/game_spec.js
@@ -1,9 +1,16 @@
 import Game from '../lib/game';
+import { expect } from 'chai';
 
 describe('Bowling Game', function() {
 
-  it("creates a new game", () => {
+  it("scores a gutter game", () => {
     let game = new Game()
+
+    for (let i=0; i < 20; i++) {
+      game.roll(0)
+    }
+
+    expect(game.score()).to.eq(0)
   })
 
 })
