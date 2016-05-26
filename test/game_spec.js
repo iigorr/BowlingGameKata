9287bebd2ae7710fc225671eb460a8ac0cb4627d
Christoph Wagner <github@pandawhisperer.net>
2016-05-26 09:55:03 -0700

    Simplify second test

--- test/game_spec.js
+++ test/game_spec.js
@@ -2,25 +2,24 @@ import Game from '../lib/game';
 import { expect } from 'chai';
 
 describe('Bowling Game', function() {
-
-  it("scores a gutter game", () => {
-    let game = new Game()
-
-    for (let i=0; i < 20; i++) {
-      game.roll(0)
-    }
-
-    expect(game.score()).to.eq(0)
+  beforeEach(function() {
+    this.game = new Game()
   })
 
-  it("scores a game of all ones", () => {
-    let game = new Game()
-
-    for (let i=0; i < 20; i++) {
-      game.roll(1)
+  function rollMany(n, pins) {
+    for (let i=0; i < n; i++) {
+      this.game.roll(pins)
     }
+  }
 
-    expect(game.score()).to.eq(20)
+  it("scores a gutter game", function() {
+    rollMany.call(this, 20, 0)
+    expect(this.game.score()).to.eq(0)
+  })
+
+  it("scores a game of all ones", function() {
+    rollMany.call(this, 20, 1)
+    expect(this.game.score()).to.eq(20)
   })
 
 })
